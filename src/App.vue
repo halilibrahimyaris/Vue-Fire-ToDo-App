b99eb16fb2fdc6b5c08ada1082c7d2639eb19892
halilyaris <halilibrahim.yaris@stu.fsm.edu.tr>
2020-11-23 10:17:16 +0300

    Vue Fire ToDo App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+       <h2>VueFire ToDo</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <ToDoList/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ToDoList from './components/ToDoList.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    ToDoList
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/firebase/db.js
@@ -0,0 +1,4 @@
+//import firebase from 'firebase';
+//import 'firebase/firestore';
+
+// Please open top comments .Your web app's Firebase configuration should be bottom.
